7fb0dbabf654f784af77ba8a10ac5066404ff0b9
Talha Ayub <khantalharahi@gmail.com>
2024-07-01 01:08:55 +0500

    add the socket io library

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,49 @@
 import dotenv from "dotenv";
-dotenv.config();
 
 import connectDB from "./src/db/connect.js";
 import { app } from "./src/app.js";
 import { Server } from "socket.io";
+import { createServer } from "http";
+import { v4 as uuid } from "uuid";
+import { NEW_MESSAGE } from "./src/constants/events.js";
 
-// import { createUser } from "./src/seeders/userSeeders.js";
-// import {
-//   createGroupChats,
-//   createMessageInChats,
-//   createSingleChats,
-// } from "./src/seeders/chatSeeders.js";
-
+dotenv.config();
 const PORT = process.env.PORT || 8000;
 
 // Create HTTP server and integrate with Socket.IO
-const io = new Server(app, {});
+const server = createServer(app);
+const io = new Server(server, {});
+
+io.on("connection", (socket) => {
+  // console.log("Connection established", socket.id);
+
+  socket.on(NEW_MESSAGE, ({ chatId, members, message }) => {
+    const tempUser = {
+      _id: "232&asd£*&^3",
+      name: "Test User",
+    };
+
+    const messageForRealTime = {
+      content: message,
+      _id: uuid(),
+      sender: {
+        _id: tempUser._id,
+        name: tempUser.name,
+      },
+      chat: chatId,
+      createdAt: new Date().toISOString(),
+    };
+    console.log("New message", messageForRealTime);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
--- backend/src/constants/events.js
+++ backend/src/constants/events.js
@@ -6,4 +6,13 @@ const NEW_MESSAGE_ALERT = "new-message-alert";
 
 const NEW_REQUEST = "new-request";
 
-export { ALERT, REFETCH_ALERT, NEW_ATTACHMENT, NEW_MESSAGE_ALERT, NEW_REQUEST };
+const NEW_MESSAGE = "new-message";
+
+export {
+  ALERT,
+  REFETCH_ALERT,
+  NEW_ATTACHMENT,
+  NEW_MESSAGE_ALERT,
+  NEW_REQUEST,
+  NEW_MESSAGE,
+};
